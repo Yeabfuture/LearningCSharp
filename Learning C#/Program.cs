cecb813e781538fb109592d73b486f2a3522ed95
Yeabsira <yeabsiramersha58@yeabfuture.com>
2024-03-26 13:01:58 +0300

    testing

--- Learning C#/Program.cs
+++ Learning C#/Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 
-class Program
+namespace calab // namespaces are groups of classess which used for organization fo the files and differentation
 {
-    public static void Main(string[] args)
+    class Program // class is a template which contain various members as methdos and variables
     {
-        Console.WriteLine("Hello");
-    }
+        public static void Main(string[] args) // This is where excution starts // Static Methods belong to the class itself and don't depend on the instances so they can be called directly.
+        {
+            Console.WriteLine("Hello"); // argument in the method is called parameters.
+           var value = Console.ReadLine(); // var keyword is useful when we don't know the type of data we are working on and it will assign the datatyp ebased on the input value
+           
+        }
 
+    }
 }
+
+
